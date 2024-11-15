3953358094cd8c896847dc580fadba759cda98f3
RAVINANDAN LUTUKURTHI <nandan7997@gmail.com>
2024-11-15 06:45:13 +0000

    Mybtpproject

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("myfioriapp.controller.App", {
+        onInit: function() {
+        }
+      });
+    }
+  );
+  
